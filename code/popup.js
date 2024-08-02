a5709882fdd0d9c76f99596479ea4cacbd36a521
imtiazraqib <imtiazud@ualberta.ca>
2024-08-02 17:06:25 -0600

    added in progress badge for YT and PV

--- code/popup.js
+++ code/popup.js
@@ -11,17 +11,21 @@ chrome.storage.sync.get(["extensionEnabled"], function (result) {
 function setPlatformUI(platform) {
   const platformIcon = document.getElementById("platform-icon");
   const platformText = document.getElementById("platform-text");
+  const platformBadge = document.getElementById("platform-badge");
 
   switch (platform) {
     case "netflix":
+      platformBadge.classList.add("hide");
       platformIcon.src = "icons8-netflix-96.png";
       platformText.innerText = "Skip Intro, Skip Next Episode";
       break;
     case "youtube":
+      platformBadge.classList.remove("hide");
       platformIcon.src = "icons8-youtube-96.png";
       platformText.innerText = "Skip Ads";
       break;
     case "prime":
+      platformBadge.classList.remove("hide");
       platformIcon.src = "icons8-amazon-prime-video-96.png";
       platformText.innerText = "Skip Ads";
       break;
